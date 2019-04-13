b4fb1835139384c43156c17d992a223f1005102e
unknown <kparker4089@myhta.org>
2019-04-12 14:00:22 -1000

    project complete

--- assets/stretch-goal.js
+++ assets/stretch-goal.js
@@ -1,32 +1,44 @@
-window.onload = function(){
+window.onload = function()
+{
+    var dirt = document.getElementsByClassName('dirt');
+    var sound = document.getElementById("sound"); 
+    var grid = document.getElementById("dirt-box"); 
+    
+    var score = 0;    
+    
+    for (let i = 0; i < dirt.length; i++)
+    {
+        dirt[i].addEventListener("click", function click() 
+        {
+            if (dirt[i].innerHTML)
+            {
+                sound.play();
+                dirt[i].innerHTML = "";
+                score++;
+                document.getElementById("score").innerHTML = "Score: " + score;
 
-  var dirt = document.getElementsByClassName('dirt');
-
-  var sound = document.getElementById("sound");
-  var grid = document.getElementById("dirtbox");
-  var score = 0;
-
-  for(let i = 0; i < dirt.length; i++){
-      dirt[i].addEventListener("click", function(){
-              if (dirt[i].innerHTML){
-                  sound.play();
-                  dirt[i].innerHTML = "";
-                  score++;
-                  document.getElementById("score").innerHTML = "Score: " + score;
-              } else {
-                  console.log("nothing is in there");
-              }
-          });
-      };
+            }
+            else
+            {
+                console.log("Nothing is there.");
+            }
+        });
+    };
 
 
-setInterval(function setMole(){
-  var mole = document.createElement("div");
-  mole.setAttribute("id", "mole");
-  var randInt = Math.floor(Math.random() * Math.floor(dirt.length));
-  if (dirt[randInt].innerHTML === ""){
-      dirt[randInt].appendChild(mole);
-  } else {
-      console.log("There are too many moles");
-  }
-},3000)};
+setInterval (function setMole()
+{
+    var mole = document.createElement("div");
+    mole.setAttribute("class", "mole");
+    var random = Math.floor(Math.random() * Math.floor(dirt.length));
+    
+    if (dirt[random].innerHTML === "")
+    {
+        dirt[random].appendChild(mole);
+    }
+    else
+    {
+        console.log("There are too many moles!");
+    }
+}, 750);
+};
